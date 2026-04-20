12a3ec7588d7ca406950ce449a6b677adf11c9f5
Vishv-codes <vishu30.vp@gmail.com>
2026-04-20 23:06:01 +1000

    First working draft

--- scripts/components/coin.gd
+++ scripts/components/coin.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+@onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 func _on_body_entered(body: Node2D) -> void:
-	print("+1 Coin")
-	queue_free()
+	game_manager.add_point()
+	animation_player.play("pickup")
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
@@ -11,12 +12,28 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	# Play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+	
+	 #apply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
